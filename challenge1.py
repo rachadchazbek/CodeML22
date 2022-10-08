f74d0185f150749dae7690861ffc7ff1c42b9b97
se-master <64152321+swemaster@users.noreply.github.com>
2022-10-08 15:43:00 -0400

    split train data in x and y

--- challenge1.py
+++ challenge1.py
@@ -1,13 +1,21 @@
 # importing libraries
 import pandas as pd
+import tensorflow as tf
  
 # need to modify the path based on the machine
 df = pd.read_csv("CodeML22 datasets\language-identification-datasets.csv")
 
 # split dataframe in 2 sets
+train = df[df['Language'].notnull()]
+test = df[df['Language'].isnull()]
 
-trainingDataset = df[df['Language'].notnull()]
-evaluationDataset = df[df['Language'].isnull()]
+# splits the train data into x
+train_x = train.drop("ID", axis = 1)
+train_x = train_x.drop("Language", axis = 1)
+# splits the train data into y
+train_y = train.drop("ID", axis = 1)
+train_y = train_y.drop("Text", axis = 1)
 
-print(trainingDataset)
-print(evaluationDataset)
+print(train_x)
+print(train_y)
+
